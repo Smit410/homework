329b9800fa7d4e598d733fbe2655c09c397f9e44
Smit410 <smit.shah.chintan@gmail.com>
2026-01-05 19:09:50 +1300

    added alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,21 @@
+ch = input("Enter a character: ")
+
+if ch == 'A' or ch == 'B' or ch == 'C' or ch == 'D' or ch == 'E' or ch == 'F' or ch == 'G' or \
+   ch == 'H' or ch == 'I' or ch == 'J' or ch == 'K' or ch == 'L' or ch == 'M' or ch == 'N' or \
+   ch == 'O' or ch == 'P' or ch == 'Q' or ch == 'R' or ch == 'S' or ch == 'T' or ch == 'U' or \
+   ch == 'V' or ch == 'W' or ch == 'X' or ch == 'Y' or ch == 'Z' or \
+   ch == 'a' or ch == 'b' or ch == 'c' or ch == 'd' or ch == 'e' or ch == 'f' or ch == 'g' or \
+   ch == 'h' or ch == 'i' or ch == 'j' or ch == 'k' or ch == 'l' or ch == 'm' or ch == 'n' or \
+   ch == 'o' or ch == 'p' or ch == 'q' or ch == 'r' or ch == 's' or ch == 't' or ch == 'u' or \
+   ch == 'v' or ch == 'w' or ch == 'x' or ch == 'y' or ch == 'z':
+    print("It is a letter.")
+
+elif ch == '0' or ch == '1' or ch == '2' or ch == '3' or ch == '4' or \
+     ch == '5' or ch == '6' or ch == '7' or ch == '8' or ch == '9':
+    print("It is a number.")
+
+else:
+    print("It is not a letter nor a number.")
+# I didnt know any other way to do it
+                              
+                              #ps. this took F O R E V E R ! ! ! !
