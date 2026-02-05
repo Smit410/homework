d603642919de8af728fa7b5245379ed1f351a310
Smit410 <smit.shah.chintan@gmail.com>
2026-02-06 10:43:00 +1300

    added reverse.py

---
+++ reverse.py
@@ -0,0 +1,17 @@
+num = int(input("Enter a number: "))
+
+
+if num < 0:
+    num = -num
+
+count = 0
+
+
+if num == 0:
+    count = 1
+else:
+    while num > 0:
+        num = num // 10   # Remove last digit
+        count += 1
+
+print("Total digits:", count)
