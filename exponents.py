5c1daff074c0bceb1c5d8db2f9a02be9060ae3b6
Smit410 <smit.shah.chintan@gmail.com>
2026-03-21 15:00:25 +1300

    added exponents.py

--- exponents.py
+++ exponents.py
@@ -1,4 +1,9 @@
-num = float(input("Enter number: "))
-pwr = float(input("Enter power: "))
+base = int(input("Enter base: "))
+exp = int(input("Enter exponent: "))
 
-print("Result:", num ** pwr)
+result = 1
+
+for i in range(exp):
+    result = result * base
+
+print("Result:", result)
