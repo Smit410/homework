533b6eaabeaf50dde4ff1db1ae0aa6f6daed4e23
Smit410 <smit.shah.chintan@gmail.com>
2026-03-29 10:07:15 +1300

    added exponents.py

--- exponents.py
+++ exponents.py
@@ -1,9 +1,6 @@
-base = int(input("Enter base: "))
-exp = int(input("Enter exponent: "))
-
+base = int(input("Enter the base number: "))
+exponent = int(input("Enter the power (n): "))
 result = 1
-
-for i in range(exp):
+for i in range(exponent):
     result = result * base
-
-print("Result:", result)
+print(f"{base} raised to the power of {exponent} is: {result}")
