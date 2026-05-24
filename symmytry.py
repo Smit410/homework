a72f693cdc6015df88de2d048361f8e8b4f3661e
Smit410 <smit.shah.chintan@gmail.com>
2026-05-24 17:43:32 +1200

    added symytry.py

---
+++ symmytry.py
@@ -0,0 +1,13 @@
+set1_a = {'blue', 'green'}
+set2_a = {'blue', 'yellow'}
+result_a = set1_a.symmetric_difference(set2_a)
+
+print(f"A. Symmetric Difference: {result_a}")
+
+
+
+set1_b = {1, 2, 3, 4, 5}
+set2_b = {1, 5, 6, 7, 8, 9}
+result_b = set1_b.symmetric_difference(set2_b)
+
+print(f"B. Symmetric Difference: {result_b}")
