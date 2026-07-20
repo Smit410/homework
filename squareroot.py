c18d3f316e6218febfed4f05ef9ff1ebb608d097
Smit410 <smit.shah.chintan@gmail.com>
2026-07-20 12:55:24 +1200

    added squareroot.py

--- squareroot.py
+++ squareroot.py
@@ -1,3 +1,9 @@
-num= int(input("plz put number: "))
-sqro= num**0.5
-print("the square root of ur number is: ", sqro)
+# Get input from the user
+base = int(input("Enter the base number: "))
+n = int(input("Enter the number of powers (n): "))
+result = 1
+for i in range(n):
+    result = result * base
+    power = i + 1
+    # Print the result without using f-strings
+    print(base, "raised to the power of", power, "is", result)
