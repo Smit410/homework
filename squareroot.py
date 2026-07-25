b0018d713f96627583f54e5c06c121c29dfa7969
Smit410 <smit.shah.chintan@gmail.com>
2026-07-25 12:36:45 +1200

    added dquareroot.py

--- squareroot.py
+++ squareroot.py
@@ -1,9 +1,7 @@
-# Get input from the user
 base = int(input("Enter the base number: "))
 n = int(input("Enter the number of powers (n): "))
 result = 1
 for i in range(n):
     result = result * base
     power = i + 1
-    # Print the result without using f-strings
     print(base, "raised to the power of", power, "is", result)
